695a8f11aad7d5065a1a5eceeb07aba4f651fb8a
Francisco Timez <franciscotimez@gmail.com>
2023-02-27 08:45:14 -0300

    Static render image

--- 02-pokemon-static/components/ui/NavBar.tsx
+++ 02-pokemon-static/components/ui/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Text, useTheme, Spacer } from "@nextui-org/react";
+import Image from "next/image";
 
 export const NavBar = () => {
   const { theme } = useTheme();
@@ -15,6 +16,12 @@ export const NavBar = () => {
         backgroundColor: theme?.colors.gray100.value,
       }}
     >
+      <Image
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+        alt="icono de la app"
+        width={70}
+        height={70}
+      />
       <Text color="white" h2>
         P
       </Text>
