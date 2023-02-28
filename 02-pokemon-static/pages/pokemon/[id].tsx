8e8f5a666d8191f810aa96b275c4a64ccf4bb120
Francisco Timez <franciscotimez@gmail.com>
2023-02-28 20:26:59 -0300

    revalidate

--- 02-pokemon-static/pages/pokemon/[id].tsx
+++ 02-pokemon-static/pages/pokemon/[id].tsx
@@ -114,7 +114,6 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 };
 
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
-
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
   const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
 
@@ -131,12 +130,12 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 //- The data comes from a headless CMS.
 //- The data can be publicly cached (not user-specific).
 //- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
-
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
 
   return {
     props: { pokemon: await getPokemonInfo(id) },
+    revalidate: 86400, // Segundos 60*60*24
   };
 };
 
