8efac7881f8c07aa83116cedb34031b7b3d71888
Francisco Timez <franciscotimez@gmail.com>
2023-03-03 10:34:06 -0300

    Add mongodb connection

---
+++ 03-open-jira/database/db.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+
+const mongooConnection = {
+  isConnected: 0
+};
+
+export const connect = async () => {
+  if (mongooConnection.isConnected === 1) {
+    console.log("Ya estamos conectados");
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongooConnection.isConnected = mongoose.connections[0].readyState;
+
+    if (mongooConnection.isConnected === 1) {
+      console.log('Usando conexion anterior');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  await mongoose.connect('....');
+  mongooConnection.isConnected = 1;
+  console.log('Conectado a mongoDB: ');
+};
+
+export const disconnect = async () => {
+  if (mongooConnection.isConnected !== 0) return;
+  
+  await mongoose.disconnect();
+  console.log('Desconectado de mongoDB');
+};
