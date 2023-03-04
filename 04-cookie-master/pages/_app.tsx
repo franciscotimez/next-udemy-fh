23fe4c44d7db568701f9f5cd72ca93d87521b38b
Francisco Timez <franciscotimez@gmail.com>
2023-03-04 19:24:46 -0300

    Se soluciona lo de el theme, piola...

--- 04-cookie-master/pages/_app.tsx
+++ 04-cookie-master/pages/_app.tsx
@@ -1,29 +1,46 @@
 import "../styles/globals.css";
 import type { AppContext, AppProps } from "next/app";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { darkTheme } from "../themes";
+import { CssBaseline, ThemeProvider, Theme } from "@mui/material";
+import { darkTheme, lightTheme, customTheme } from "../themes";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
+const themes: { [any: string]: Theme } = {
+  dark: darkTheme,
+  light: lightTheme,
+  custom: customTheme,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [currentTheme, setCurrentTheme] = useState(themes.light);
+
+  useEffect(() => {
+    const cookieTheme = Cookies.get("theme") || "light";
+    const selectedTheme = themes[cookieTheme];
+    setCurrentTheme(selectedTheme);
+  }, []);
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
 
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  const { theme } = appContext.ctx.req
-    ? (appContext.ctx.req as any).cookies
-    : { theme: "light" };
+// // Deshabilita la optimizacion estatica, TRATAR DE NO USAR, tambien se puede usar por page
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   const { theme } = appContext.ctx.req
+//     ? (appContext.ctx.req as any).cookies
+//     : { theme: "light" };
 
-  const validThemes = ["light", "dark", "custom"];
+//   const validThemes = ["light", "dark", "custom"];
 
-  console.log("GetInitialProps", { theme });
+//   // console.log("GetInitialProps", { theme });
 
-  return {
-    theme: validThemes.includes(theme) ? theme : "dark",
-  };
-};
+//   return {
+//     theme: validThemes.includes(theme) ? theme : "dark",
+//   };
+// };
 
 export default MyApp;
