fb53eb586de86a91a693532fb7fb13b96325251e
Francisco Timez <franciscotimez@gmail.com>
2023-03-09 18:23:51 -0300

    pagina 404 custom

---
+++ 05-teslo-shop/pages/404.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ShopLayout } from "../components/layouts";
+import { Box, Typography } from "@mui/material";
+
+const Custom404 = () => {
+  return (
+    <ShopLayout
+      title="Page not found"
+      pageDescription="No hay nada que mostrar aqui"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 200px)"
+        sx={{ flexDirection: { xs: "column", sm: "row" } }}
+      >
+        <Typography variant="h1" component="h1" fontSize={80} fontWeight={200}>
+          404 |
+        </Typography>
+        <Typography marginLeft={2}>No Encontramos nada.</Typography>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default Custom404;
