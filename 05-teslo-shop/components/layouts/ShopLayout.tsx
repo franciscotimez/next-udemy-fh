e0e4ee76609602b06a0c15c32dbf70155fc97ef1
Francisco Timez <franciscotimez@gmail.com>
2023-03-09 14:43:42 -0300

    Definimos layout

---
+++ 05-teslo-shop/components/layouts/ShopLayout.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+}
+
+export const ShopLayout: React.FunctionComponent<Props> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={pageDescription} />
+
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={pageDescription} />
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+      </Head>
+      <nav></nav>
+
+      <main
+        style={{ margin: "80px auto", maxWidth: "1440px", padding: "0px 30px" }}
+      >
+        {children}
+      </main>
+      <footer></footer>
+    </>
+  );
+};
--- 05-teslo-shop/pages/index.tsx
+++ 05-teslo-shop/pages/index.tsx
@@ -1,9 +1,22 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { Typography } from "@mui/material";
+
+import { ShopLayout } from "../components/layouts";
 
 const Home: NextPage = () => {
   return (
-    <h1>Test - Shop</h1>
-  )
-}
+    <ShopLayout
+      title={"Teslo-Shop - Home"}
+      pageDescription={"Encuentra los mejores productos de Teslo aqui!"}
+    >
+      <Typography variant="h1" component="h1">
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Tienda
+      </Typography>
+    </ShopLayout>
+  );
+};
 
-export default Home
+export default Home;
