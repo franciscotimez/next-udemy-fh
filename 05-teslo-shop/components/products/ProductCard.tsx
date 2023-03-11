71f2b128677f938205334caac3c3343fee002670
Francisco Timez <franciscotimez@gmail.com>
2023-03-11 12:44:07 -0300

    Hover in image

--- 05-teslo-shop/components/products/ProductCard.tsx
+++ 05-teslo-shop/components/products/ProductCard.tsx
@@ -1,22 +1,49 @@
-import { Card, CardActionArea, CardMedia, Grid } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React, { useMemo, useState } from "react";
 import { IProduct } from "../../interfaces";
 
 interface Props {
   product: IProduct;
 }
 export const ProductCard: React.FunctionComponent<Props> = ({ product }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
+
   return (
-    <Grid item xs={6} sm={4}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <Card>
         <CardActionArea>
           <CardMedia
             component="img"
-            image={`products/${product.images[0]}`}
+            className="fadeIn"
+            image={productImage}
             alt={product.title}
+            // onLoad={}
           />
         </CardActionArea>
       </Card>
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>${product.price}</Typography>
+      </Box>
     </Grid>
   );
 };
