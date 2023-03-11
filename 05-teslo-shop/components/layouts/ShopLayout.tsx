8a35178c871d75eb74540570f4ecde7f014a7a0c
Francisco Timez <franciscotimez@gmail.com>
2023-03-11 12:09:12 -0300

    SideMenu

--- 05-teslo-shop/components/layouts/ShopLayout.tsx
+++ 05-teslo-shop/components/layouts/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import React from "react";
-import { Navbar } from "../ui";
+import { Navbar, SideMenu } from "../ui";
 
 interface Props {
   children: React.ReactNode;
@@ -28,6 +28,8 @@ export const ShopLayout: React.FunctionComponent<Props> = ({
       <nav>
         <Navbar />
       </nav>
+
+      <SideMenu />
 
       <main
         style={{ margin: "80px auto", maxWidth: "1440px", padding: "0px 30px" }}
