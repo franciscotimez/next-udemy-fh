e526b943b97c937e8bbef9adf388f22ea2f60a74
Francisco Timez <franciscotimez@gmail.com>
2023-03-11 15:38:04 -0300

    address page

---
+++ 05-teslo-shop/pages/checkout/address.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { ShopLayout } from "../../components/layouts/ShopLayout";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const AddressPage = () => {
+  return (
+    <ShopLayout
+      title="Direccion"
+      pageDescription="Confirmar direccion del destino"
+    >
+      <Typography variant="h1" component="h1">
+        Direccion
+      </Typography>
+
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid item xs={12} sm={6}>
+          <TextField label="Nombre" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField label="Apellido" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField label="Direccion" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Direccion 2 (opcional)"
+            variant="filled"
+            fullWidth
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField label="Codigo Postal" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField label="Ciudad" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <Select variant="filled" label="Pais" value={1}>
+              <MenuItem value={1}>Argentina</MenuItem>
+              <MenuItem value={2}>Honduras</MenuItem>
+              <MenuItem value={3}>Canada</MenuItem>
+              <MenuItem value={4}>Brasil</MenuItem>
+              <MenuItem value={5}>Mexico</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField label="Telefono" variant="filled" fullWidth />
+        </Grid>
+      </Grid>
+
+      <Box sx={{ mt: 5 }} display="flex" justifyContent="end">
+        <Button color="secondary" className="circular-btn" size="large">
+          Revisar Pedido
+        </Button>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default AddressPage;
