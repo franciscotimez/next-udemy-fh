ee1b0869a171b1f365c453a034dfef133cad31d7
Francisco Timez <franciscotimez@gmail.com>
2023-03-11 11:42:04 -0300

    home page

--- 05-teslo-shop/pages/index.tsx
+++ 05-teslo-shop/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 import { ShopLayout } from "../components/layouts";
+import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
   return (
@@ -13,8 +20,24 @@ const Home: NextPage = () => {
         Tienda
       </Typography>
       <Typography variant="h2" sx={{ mb: 1 }}>
-        Tienda
+        Todos los productos
       </Typography>
+
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item xs={6} sm={4} key={product.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={`products/${product.images[0]}`}
+                  alt={product.title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   );
 };
