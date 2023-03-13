0aa87ab02038f58acfb99e51933d264a403f05da
Francisco Timez <franciscotimez@gmail.com>
2023-03-13 12:05:12 -0300

    mongo product Schema and model

---
+++ 05-teslo-shop/models/Product.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, model, Model } from "mongoose";
+import { IProduct } from '../interfaces/products';
+
+const productSchema = new Schema({
+  description: { type: String, require: true },
+  images: [{ type: String }],
+  inStock: { type: Number, require: true, default: 0 },
+  price: { type: Number, require: true, default: 0 },
+  sizes: [{ type: String, enum: { values: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'], message: '{VALUE} no es un tamano valido' } }],
+  slug: { type: String, require: true, unique: true },
+  tags: [{ type: String }],
+  title: { type: String, require: true },
+  type: { type: String, enum: { values: ['shirts', 'pants', 'hoodies', 'hats'], message: '{VALUE} no es un tipo valido' } },
+  gender: { type: String, enum: { values: ['men', 'women', 'kid', 'unisex'], message: '{VALUE} no es un genero valido' } },
+}, {
+  timestamps: true
+});
+
+// ToDo: Crear indice
+
+const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema);
+
+export default Product;
