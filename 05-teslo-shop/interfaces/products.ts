222450ba43495f31be1a1fe2fd43b7b4cc70caf1
Francisco Timez <franciscotimez@gmail.com>
2023-03-13 12:16:21 -0300

    Create database connection and seed db

--- 05-teslo-shop/interfaces/products.ts
+++ 05-teslo-shop/interfaces/products.ts
@@ -10,6 +10,10 @@ export interface IProduct {
   title: string;
   type: IType;
   gender: 'men' | 'women' | 'kid' | 'unisex';
+
+  // cosas de mongo
+  createdAt: string;
+  updatedAt: string;
 }
 
 export type ISize = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
