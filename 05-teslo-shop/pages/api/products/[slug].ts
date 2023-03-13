be925500ff55ee6a67bc6e65ffb83fcf1b6de6b2
Francisco Timez <franciscotimez@gmail.com>
2023-03-13 12:52:17 -0300

    get product by slug

---
+++ 05-teslo-shop/pages/api/products/[slug].ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { Product } from '../../../models';
+import { IProduct } from '../../../interfaces/products';
+
+type Data =
+  | { message: string; }
+  | IProduct;
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  switch (req.method) {
+    case 'GET':
+      return getProductBySlug(req, res);
+
+    default:
+      res.status(400).json({ message: 'BadRequest' });
+      break;
+  }
+}
+
+const getProductBySlug = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { slug } = req.query;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+
+  if (!product) return res.status(404).json({ message: 'Product not Found' });
+  return res.status(200).json(product);
+
+};
