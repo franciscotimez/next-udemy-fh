763222e21b29e0ff9b9d1f0151eb78884653083a
Francisco Timez <franciscotimez@gmail.com>
2023-03-16 09:07:15 -0300

    Add cart Context

--- 05-teslo-shop/pages/_app.tsx
+++ 05-teslo-shop/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { SWRConfig } from "swr";
 import { lightTheme } from "../themes";
-import { UiProvider } from "../context";
+import { UiProvider, CartProvider } from "../context";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,12 +13,14 @@ function MyApp({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <UiProvider>
-        <ThemeProvider theme={lightTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </UiProvider>
+      <CartProvider>
+        <UiProvider>
+          <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UiProvider>
+      </CartProvider>
     </SWRConfig>
   );
 }
