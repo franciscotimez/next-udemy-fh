2c5141f0750aebf5732d27d5a536c051d69b611f
Francisco Timez <franciscotimez@gmail.com>
2023-03-18 14:53:07 -0300

    fix token validate

--- 05-teslo-shop/context/auth/AuthProvider.tsx
+++ 05-teslo-shop/context/auth/AuthProvider.tsx
@@ -28,6 +28,8 @@ export const AuthProvider: React.FunctionComponent<Props> = ({ children }) => {
   }, []);
 
   const checkToken = async () => {
+    if (!Cookies.get("token")) return;
+
     try {
       const { data } = await tesloApi.get("/user/validate-token");
       const { token, user } = data;
