87de12709f60ca51b02189f4e74e2063d0ea06aa
Francisco Timez <franciscotimez@gmail.com>
2023-03-21 10:07:01 -0300

    Arreglo interfaces

--- 05-teslo-shop/context/cart/CartProvider.tsx
+++ 05-teslo-shop/context/cart/CartProvider.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useReducer, useState } from "react";
 import Cookies from "js-cookie";
 
-import { ICartProduct } from "../../interfaces";
+import { ICartProduct, ShippingAddress } from "../../interfaces";
 import { CartContext, cartReducer } from "./";
 
 export interface CartState {
@@ -13,17 +13,6 @@ export interface CartState {
   tax: number;
   total: number;
   shippingAddress?: ShippingAddress;
-}
-
-export interface ShippingAddress {
-  firstname: string;
-  lastname: string;
-  address: string;
-  address2: string;
-  zip: string;
-  city: string;
-  country: string;
-  phone: string;
 }
 
 const CART_INITIAL_STATE: CartState = {
---
+++ 05-teslo-shop/interfaces/order.ts
@@ -0,0 +1,39 @@
+import { IUser } from "./user";
+
+export interface IOrder {
+  _id?: string;
+  user?: IUser | string;
+  orderItems: IOrderItem[];
+  shippingAddress: ShippingAddress;
+  paymentResult?: string;
+  numberOfItems: number;
+  subtotal: number;
+  tax: number;
+  total: number;
+  isPaid: boolean;
+  paidAt: string;
+  // cosas de mongo
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface IOrderItem {
+  _id: string;
+  title: string;
+  size: string;
+  quantity: number;
+  slug: string;
+  image: string;
+  price: number;
+}
+
+export interface ShippingAddress {
+  firstname: string;
+  lastname: string;
+  address: string;
+  address2: string;
+  zip: string;
+  city: string;
+  country: string;
+  phone: string;
+}
