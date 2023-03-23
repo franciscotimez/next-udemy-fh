066832291a682dfed58bbbe362a38528d5bed99d
Francisco Timez <franciscotimez@gmail.com>
2023-03-23 13:57:34 -0300

    api users

---
+++ 05-teslo-shop/pages/api/admin/users.ts
@@ -0,0 +1,56 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { IUser } from '../../../interfaces';
+import { User } from '../../../models';
+import { isValidObjectId } from 'mongoose';
+
+type Data =
+  | { message: string; }
+  | IUser[];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  switch (req.method) {
+    case 'GET':
+      return getUsers(req, res);
+
+    case 'PUT':
+      return updateUser(req, res);
+
+    default:
+      return res.status(400).json({ message: 'Bad Request' });
+  }
+}
+
+const getUsers = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+  await db.connect();
+  const users = await User.find().select('-password').lean();
+  await db.disconnect();
+
+  return res.status(200).json(users);
+};
+
+const updateUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { userId = '', role = '' } = req.body;
+
+  if (!isValidObjectId(userId))
+    return res.status(400).json({ message: 'Bad Request' });
+
+  const validRoles = ['admin', 'client', 'super-user', 'SEO'];
+  if (!validRoles.includes(role))
+    return res.status(400).json({ message: 'Bad Request' });
+
+  await db.connect();
+  const user = await User.findById(userId);
+  if (!user) {
+    await db.disconnect();
+    return res.status(400).json({ message: 'Bad Request' });
+  }
+
+  user.role = role;
+  await user.save();
+  await db.disconnect();
+  return res.status(200).json({ message: 'Usuario actualizado' });
+
+};
