75ba79ac704f24cdeb9d2ef9493f59a5bb63f408
Francisco Timez <franciscotimez@gmail.com>
2023-03-23 13:32:41 -0300

    middlewares

--- 05-teslo-shop/middleware.ts
+++ 05-teslo-shop/middleware.ts
@@ -11,15 +11,33 @@ export async function middleware(req: NextRequest) {
 
   if (!session) {
     const requestedPage = req.nextUrl.pathname;
+    if (req.nextUrl.pathname.startsWith('/api'))
+      return new NextResponse(JSON.stringify({ message: 'authentication failed' }),
+        { status: 401, headers: { 'content-type': 'application/json' } });
+
     const url = req.nextUrl.clone();
     url.pathname = '/auth/login';
     url.search = `p=${requestedPage}`;
     return NextResponse.redirect(url);
+  }
+
+  if (req.nextUrl.pathname.startsWith('/admin') || req.nextUrl.pathname.startsWith('/api')) {
+    const validRoles = ['admin', 'super-user', 'SEO'];
+
+    if (!validRoles.includes((session.user as any).role)) {
+      if (req.nextUrl.pathname.startsWith('/api'))
+        return new NextResponse(JSON.stringify({ message: 'authentication failed' }),
+          { status: 401, headers: { 'content-type': 'application/json' } });
+
+      const url = req.nextUrl.clone();
+      url.pathname = '/';
+      return NextResponse.redirect(url);
+    }
   }
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/checkout/:path*'],
+  matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*'],
 };
