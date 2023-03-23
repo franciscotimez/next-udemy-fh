d9c0d588fe928ba81a4433c3a7f2404a2ac95afc
Francisco Timez <franciscotimez@gmail.com>
2023-03-23 15:28:07 -0300

    orders show data

--- 05-teslo-shop/pages/admin/orders.tsx
+++ 05-teslo-shop/pages/admin/orders.tsx
@@ -1,15 +1,49 @@
-import { ConfirmationNumberOutlined, PeopleOutlined } from "@mui/icons-material";
-import { Grid, MenuItem, Select, Typography } from "@mui/material";
+import {
+  ConfirmationNumberOutlined,
+  PeopleOutlined,
+} from "@mui/icons-material";
+import { Chip, Grid, MenuItem, Select, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 import { AdminLayout } from "../../components/layouts";
-import { IUser } from "../../interfaces";
+import { IOrder, IUser } from "../../interfaces";
 import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import { tesloApi } from "../../api";
 
+const columns: GridColDef[] = [
+  { field: "id", headerName: "Order ID", width: 250 },
+  { field: "email", headerName: "Correo", width: 250 },
+  { field: "name", headerName: "Nombre Completo", width: 200 },
+  { field: "total", headerName: "Monto total" },
+  {
+    field: "isPaid",
+    headerName: "Pagado",
+    renderCell: ({ row }: GridRenderCellParams) => {
+      return row.isPaid ? (
+        <Chip variant="outlined" label="Pagado" color="success" />
+      ) : (
+        <Chip variant="outlined" label="Pendiente" color="error" />
+      );
+    },
+  },
+  { field: "numberOfItems", headerName: "No Items", align: "center" },
+  {
+    field: "check",
+    headerName: "ver Orden",
+    renderCell: ({ row }: GridRenderCellParams) => {
+      return (
+        <a href={`/admin/orders/${row.id}`} target="_blank">
+          Ver Orden
+        </a>
+      );
+    },
+  },
+  { field: "updatedAt", headerName: "Actualizada el", width: 200 },
+];
+
 const OrdersPage: NextPage = () => {
-  const { data, error, mutate } = useSWR<IUser[]>("/api/admin/users");
+  const { data, error, mutate } = useSWR<IOrder[]>("/api/admin/orders");
 
   if (!error && !data) {
     return <></>;
@@ -31,37 +65,15 @@ const OrdersPage: NextPage = () => {
     }
   };
 
-  const columns: GridColDef[] = [
-    { field: "email", headerName: "Correo", width: 250 },
-    { field: "name", headerName: "Nombre Completo", width: 300 },
-    {
-      field: "role",
-      headerName: "Rol",
-      width: 300,
-      renderCell: ({ row }: GridRenderCellParams) => {
-        return (
-          <Select
-            value={row.role}
-            label="Rol"
-            sx={{ width: "300px" }}
-            onChange={({ target }) => onRoleUpdated(row.id, target.value)}
-          >
-            {["admin", "client", "super-user", "SEO"].map((item) => (
-              <MenuItem key={item} value={item}>
-                {item}
-              </MenuItem>
-            ))}
-          </Select>
-        );
-      },
-    },
-  ];
-
-  const rows = data!.map((user) => ({
-    id: user._id,
-    email: user.email,
-    name: user.name,
-    role: user.role,
+  const rows = data!.map((order) => ({
+    id: order._id,
+    email: (order.user as IUser).email,
+    name: (order.user as IUser).name,
+    total: order.total,
+    isPaid: order.isPaid,
+    numberOfItems: order.numberOfItems,
+    check: order,
+    updatedAt: order.updatedAt,
   }));
 
   return (
